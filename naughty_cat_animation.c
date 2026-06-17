634af9da1fda78b326f923ee95c6fd46cc6452f2
xamdalf <xillion404@gmail.com>
2026-06-17 13:20:43 +1000

    it's workinggit add naughty_cat_daemon.c naughty_cat_animation.cgit add naughty_cat_daemon.c naughty_cat_animation.c! ahhhhhhh i love itttttttt. need to reposition and add ability to move the sprite around using the cursor when toggled into movement mode

--- naughty_cat_animation.c
+++ naughty_cat_animation.c
@@ -26,86 +26,136 @@ artist: unkown  [ASCII art archive]
 #include <sys/socket.h> //used for socket to communicate with the animation layer
 #include <sys/un.h>
 #include <unistd.h> //used for unlink(), close(), and write() functions
+#include <glib-unix.h>
+#include <gtk4-layer-shell/gtk4-layer-shell.h>
+#include <gtk/gtk.h>
 #include "naughty_cat.h"
 
 #define EXT_ERR_TERMINATED -1 //error code for when the program is terminated by a signal
+#define CAT_WIDTH 432
+#define CAT_HEIGHT 180
 
 
-volatile sig_atomic_t running = true;
+volatile int last_paw = 0;
+int data_socket;
+
+
 
 void handle_signal(int sig) {
     if (sig == SIGINT) {
-        running = false; //set the running flag to false to indicate that the program should terminate
+        printf("Program terminated by signal\n");
+        g_application_quit(g_application_get_default());
     }
 }
 
 
-int main() {
-    
-    int data_socket;
+
+void set_frame(GtkWidget *picture, const char *filename) {
+    gtk_picture_set_filename(GTK_PICTURE(picture), filename);
+}
+
+gboolean on_socket_data(gint fd, GIOCondition condition, gpointer userdata) {
+    GtkWidget *picture = GTK_WIDGET(userdata);
+    key_event_t event;
+
+    int r = read(fd, &event, sizeof(event));
+    if (r <= 0) {
+        fprintf(stderr, "Daemon disconnected\n");
+        return G_SOURCE_REMOVE;
+    }
+
+    switch (event.key_boop) {
+        case 0:
+            set_frame(picture, "idle.png");
+            printf("IDLE\n");
+            break;
+        case 1:
+            last_paw = !last_paw;
+            set_frame(picture, last_paw ? "left_boop.png" : "right_boop.png");
+            
+            if (last_paw) {
+                printf("left boop\n");
+            }
+            else {
+                printf("right boop\n");
+            }
+            break;
+        case 2:
+            last_paw = !last_paw;
+            set_frame(picture, last_paw ? "left_boop.png" : "right_boop.png");
+            printf("boooop\n");
+    }
+    return G_SOURCE_CONTINUE;
+}
+
+
+static void on_activate(GtkApplication *app, gpointer user_data) {
+
+    // CSS
+    GtkCssProvider *css = gtk_css_provider_new();
+    gtk_css_provider_load_from_string(css, "window { background: transparent; }");
+    gtk_style_context_add_provider_for_display(
+        gdk_display_get_default(),
+        GTK_STYLE_PROVIDER(css),
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
+    // window
+    GtkWidget *window = gtk_application_window_new(app);
+
+    // layer shell
+    gtk_layer_init_for_window(GTK_WINDOW(window));
+    gtk_layer_set_layer(GTK_WINDOW(window), GTK_LAYER_SHELL_LAYER_OVERLAY);
+    gtk_layer_set_keyboard_mode(GTK_WINDOW(window), GTK_LAYER_SHELL_KEYBOARD_MODE_NONE);
+    gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
+    gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
+    gtk_layer_set_margin(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_BOTTOM, 0);
+
+    // image
+    GtkWidget *picture;
+    picture = gtk_picture_new_for_filename("idle.png");
+    gtk_picture_set_content_fit(GTK_PICTURE(picture), GTK_CONTENT_FIT_CONTAIN);
+    gtk_widget_set_size_request(picture, CAT_WIDTH, CAT_HEIGHT);  // half native size, keeps ratio
+    gtk_window_set_child(GTK_WINDOW(window), picture);
+
+    // watch socket
+    g_unix_fd_add(data_socket, G_IO_IN, on_socket_data, picture);
+    // click-through — empty input region
+    gtk_widget_set_can_target(window, FALSE);  // window doesn't receive input events
+    gtk_widget_set_can_focus(window, FALSE);   // window can't receive keyboard focus
+    gtk_widget_set_visible(window, TRUE);
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+
+    // socket setup — before GTK starts
     struct sockaddr_un addr;
-    int ret; //used to store return values from socket functions for error checking
-    int r;
-    int last_paw = 0;
-    key_event_t event; //used to store the simplified key event information received from the daemon
-
-
-    data_socket = socket(AF_UNIX, SOCK_STREAM, 0); //SOCK_STREAM: read bytes as they arrive
+    data_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     if (data_socket < 0) {
         fprintf(stderr, "Failed to create socket (%s)\n", strerror(errno));
         exit(EXT_ERR_TERMINATED);
     }
-
-    memset(&addr, 0, sizeof(addr)); //zero out the address struct
-    addr.sun_family = AF_UNIX; //set the address family to AF_UNIX
-
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
-    ret = connect(data_socket, (const struct sockaddr *) &addr,
-                   sizeof(addr));
-    if (ret == -1) {
+    if (connect(data_socket, (const struct sockaddr *) &addr, sizeof(addr)) == -1) {
         fprintf(stderr, "The server is down.\n");
         exit(EXIT_FAILURE);
     }
 
+    struct sigaction sa = { .sa_handler = handle_signal, .sa_flags = 0 };
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, NULL);
 
-    while(running) {
+    // GTK takes over from here
+    GtkApplication *app = gtk_application_new("cat.naughty", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 
-        r = read(data_socket, &event, sizeof(event));
-        if (r == -1) {
-            fprintf(stderr, "Failed to read (%s)\n", strerror(errno));
-            break;
-        }
-        if (r == 0) {
-            fprintf(stderr, "Daemon disconnected\n");
-            break;
-        }
-
-
-        switch (event.key_boop) {
-
-            case 0:
-                //animate idle state with occasional blinking
-                printf("IDLE\n");
-                break;
-            
-            case 1:
-                last_paw = !last_paw; //toggle between left and right boop animations
-                if (last_paw) {
-                    //animate right paw
-                    printf("left boop\n");
-                }
-                else {
-                    //animate right paw
-                    printf("right boop\n");
-                }
-                break;
-
-            default:
-                //animate idle state with occasional blinking
-                printf("weird behaviour... ?\n");
-                break;
-        }
-
-    }
-
+    close(data_socket);
+    return status;
 }
